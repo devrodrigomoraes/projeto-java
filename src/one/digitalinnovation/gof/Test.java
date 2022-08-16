3e496e29c4c9e4a2831a99fd7ee8538ab723f31c
Rodrigo J. Moraes <108589662+devrodrigomoraes@users.noreply.github.com>
2022-08-15 22:05:07 -0300

    Add files via upload

---
+++ src/one/digitalinnovation/gof/Test.java
@@ -0,0 +1,45 @@
+package one.digitalinnovation.gof;
+
+import one.digitalinnovation.gof.singleton.SingletonEager;
+import one.digitalinnovation.gof.singleton.SingletonLazy;
+import one.digitalinnovation.gof.singleton.SingletonLazyHolder;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		SingletonLazy lazy = SingletonLazy.getInstancia();
+		System.out.println(lazy);
+		lazy = SingletonLazy.getInstancia();
+		System.out.println(lazy);
+
+		SingletonEager eager = SingletonEager.getInstancia();
+		System.out.println(eager);
+		eager = SingletonEager.getInstancia();
+		System.out.println(eager);
+		
+		SingletonLazyHolder lazyHolder = SingletonLazyHolder.getInstancia();
+		System.out.println(lazyHolder);
+		lazyHolder = SingletonLazyHolder.getInstancia();
+		System.out.println(lazyHolder);
+		
+		Comportamento defensivo = new ComportamentoDefensivo();
+		Comportamento normal = new ComportamentoNormal();
+		Comportamento agressivo = new ComportamentoAgressivo();
+		
+		Robo robo = new Robo();
+		robo.setComportamento(normal);
+		
+		robo.mover();
+		robo.mover();
+		
+		Facade facade = new Facade();
+		facade.migrarCliente("José", "07120998");
+}
+
+
+		
+
+	}
+
+}
